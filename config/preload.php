f896f21e823d922fc197d7afe97e90ec5fa763df
Mohamed Sharik <116562978+Mohamedsharik180@users.noreply.github.com>
2023-05-16 15:00:17 +0400

    Delete config directory

--- config/preload.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-if (file_exists(dirname(__DIR__).'/var/cache/prod/App_KernelProdContainer.preload.php')) {
-    require_once dirname(__DIR__).'/var/cache/prod/App_KernelProdContainer.preload.php';
-}
